54678879ea3b38a67b3760609be296cf2a80e1b6
mohammadrezahq <mohammadrezah.official@gmail.com>
2026-02-04 16:57:36 +0330

    feat: implement chaching layer for axios get requests

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const cache = new Map();
+
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_APP_URL,
   timeout: 10000,
@@ -7,6 +9,27 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (config.method === "get") {
+    const key = config.url + JSON.stringify(config.params);
+    const cached = cache.get(key);
+
+    if (cached) {
+      config.adapter = () => {
+        return Promise.resolve(cached);
+      };
+    }
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use((response) => {
+  const key = response.config.url + JSON.stringify(response.config.params);
+
+  cache.set(key, response);
+  return response;
 });
 
 axiosInstance.interceptors.response.use(
